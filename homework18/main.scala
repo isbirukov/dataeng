b98bb256e37719d89e321185704c2096f7cfd25b
isbirukov <31694361+isbirukov@users.noreply.github.com>
2024-08-21 14:50:57 +0300

    Update main.scala

--- homework18/main.scala
+++ homework18/main.scala
@@ -1,1 +1,13 @@
+object StringProcessor {
+  def processStrings(strings: List[String]): List[String] = {
+    // полностью убираем код этой функции и заменяем на 1 строку
+    strings.filter(_.length > 3).map(_.toUpperCase)
+  }
 
+
+  def main(args: Array[String]): Unit = {
+    val strings = List("apple", "cat", "banana", "dog", "elephant")
+    val processedStrings = processStrings(strings)
+    println(s"Processed strings: $processedStrings")
+  }
+}
